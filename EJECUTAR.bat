2fcdc2d3f5ad42a208d7558cb2772bf13b47f3ff
talaveron18 <talaveron18@gmail.com>
2026-08-04 12:37:16 -0300

    Activar ARGOS v7

--- EJECUTAR.bat
+++ EJECUTAR.bat
@@ -1,7 +1,7 @@
 @echo off
 if exist .venv\Scripts\python.exe (
-  .venv\Scripts\python.exe main_v6.py
+  .venv\Scripts\python.exe main_v7.py
 ) else (
-  python main_v6.py
+  python main_v7.py
 )
 pause
